dcfc31cd950d8cd27555c5400272db751671f965
kousokujin <kousokujin@kousokujin.com>
2023-05-03 23:11:22 +0900

    ボリションガチャ画面まで作成

--- src/components/Gacha10/Result10View.vue
+++ src/components/Gacha10/Result10View.vue
@@ -1,57 +1,15 @@
 <template>
-    <v-container v-if="controller.last10.length > 0">
-        <div class="text-h3">10連結果</div>
-        <v-row>
-            <v-col v-for="(item,index) in controller.last10" :key="index" cols="auto">
-            <v-card width="250"
-            :class="['d-flex']"
-            :color="last10_color(item.reality)"
-            >
-                <v-card-item>
-                <v-card-subtitle>
-                    <span>{{convert_rearity(item.reality)}}</span>
-                    <span v-if="item.ceiling"> 天井</span>
-                </v-card-subtitle>
-                <v-card-title>{{item.name}}</v-card-title>
-                <v-card-subtitle>抽選回数: {{get_history(item.name).count}}</v-card-subtitle>
-                </v-card-item>
-            </v-card>
-            </v-col>
-        </v-row>
-    </v-container>
-    <v-container v-else>
-        <div class="text-h4">「ガチャを引く」ボタンを押してください。</div>
-  </v-container>
+  <Result10WeaponView v-if="controller.type == 'weapon'" :controller="controller" />
+  <Result10MatrixesView v-if="controller.type == 'matrices'" :controller="controller" />
 </template>
-
 <script>
+    import Result10MatrixesView from "./Result10MatricesView.vue"
+    import Result10WeaponView from "./Result10WeaponView.vue"
     export default {
         props: ['controller'],
-        computed: {
-            last10_color(){
-                return (reality)=>{
-                    switch(reality){
-                        case "SSR":
-                            return "yellow-lighten-2"
-                        case "SR":
-                            return "purple-lighten-4"
-                        case "R":
-                            return "light-green-lighten-4"
-                        default:
-                            return "white"
-                    }
-                }
-            },
-            get_history(){
-                return (item)=>{
-                    return this.controller.history[item]
-                }
-            },
-            convert_rearity(){
-                return (reality)=>{
-                    return this.controller.ConvertReality(reality)
-                }
-            },
+        components: {
+            "Result10MatrixesView": Result10MatrixesView,
+            "Result10WeaponView": Result10WeaponView
         }
     }
 </script>
